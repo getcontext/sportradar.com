d90488242b80a43e206234aacf2be966f9eb03d7
andrzej.salamon@gmail.com <andrzej.salamon@gmail.com>
2023-06-29 23:01:25 +0200

    improved abstract test

--- src/test/java/com/sportradar/AbstractTest.java
+++ src/test/java/com/sportradar/AbstractTest.java
@@ -3,14 +3,24 @@ package com.sportradar;
 import org.junit.Before;
 
 public abstract class AbstractTest {
-    protected static final String EXAMPLE_TEAM = "Spain";
+    protected static final String TEAM_EXAMPLE = "Spain";
+    protected static final String TEAM_HOME = "Germany";
+    protected static final String TEAM_AWAY = "Brazil";
 
     @Before
     public void setup() {
 
     }
 
-    public String getExampleTeam() {
-        return EXAMPLE_TEAM;
+    public String getTeamExample() {
+        return TEAM_EXAMPLE;
+    }
+
+    public String getTeamHome() {
+        return TEAM_HOME;
+    }
+
+    public String getTeamAway() {
+        return TEAM_AWAY;
     }
 }
