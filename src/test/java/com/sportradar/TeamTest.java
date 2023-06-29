f0f1b903d28fd78fe85ef2920587a889341e2d8a
andrzej.salamon@gmail.com <andrzej.salamon@gmail.com>
2023-06-29 22:47:57 +0200

    TeamTest.java test team

--- src/test/java/com/sportradar/TeamTest.java
+++ src/test/java/com/sportradar/TeamTest.java
@@ -1,4 +1,22 @@
 package com.sportradar;
 
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 public class TeamTest extends AbstractTest {
+    @Test
+    public void createTeam() {
+        Team team = new Team();
+
+        assertTrue(team, team instanceof Team); //team is not null (Team)
+    }
+
+    @Test
+    public void checkName() {
+        Team team = new Team();
+        team.setName(EXAMPLE_TEAM);
+
+        assertEquals(team.getName(), EXAMPLE_TEAM);
+    }
 }
