098b8133990daca22ccbeb19e5eb212b0bd42157
andrzej.salamon@gmail.com <andrzej.salamon@gmail.com>
2023-06-29 23:22:42 +0200

    match test with teams and default score

--- src/test/java/com/sportradar/MatchTest.java
+++ src/test/java/com/sportradar/MatchTest.java
@@ -16,15 +16,46 @@ public class MatchTest  extends AbstractTest {
     public void createMatchWithTeams() {
         Match match = new Match();
 
-        Team teamAway = new Team();
-        team.setName(getTeamAway()):
+        Team teamAway = getAway();
 
-        Team teamHome = new Team();
-        team.setName(getTeamHome()):
+        Team teamHome = getHome();
 
         match.addAway(teamAway);
         match.addHome(teamHome);
 
+
+
+
+
+        assertTrue(match instanceof Match); //team is not null (Team)
+    }
+
+    private Team getHome() {
+        Team teamHome = new Team();
+        team.setName(getTeamHome());
+        return teamHome;
+    }
+
+    private Team getAway() {
+        Team teamAway = new Team();
+        team.setName(getTeamAway());
+        return teamAway;
+    }
+
+    @Test
+    public void createMatchWithTeamsAndDefaultScore() {
+        Match match = new Match();
+
+        Team teamAway = new Team();
+        team.setName(getTeamAway());
+
+        Team teamHome = new Team();
+        team.setName(getTeamHome());
+
+        match.addAway(teamAway);
+        match.addHome(teamHome);
+
+        match.setScore(0,0);
 
 
         assertTrue(match instanceof Match); //team is not null (Team)
