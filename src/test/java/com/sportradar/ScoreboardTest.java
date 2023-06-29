0998d9b2bf81b868b24919a6de5ffbf6ffe62270
andrzej.salamon@gmail.com <andrzej.salamon@gmail.com>
2023-06-29 22:54:07 +0200

    create scoreboard test

--- src/test/java/com/sportradar/ScoreboardTest.java
+++ src/test/java/com/sportradar/ScoreboardTest.java
@@ -1,5 +1,15 @@
 package com.sportradar;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
 public class ScoreboardTest  extends AbstractTest {
+    @Test
+    public void createScoreboard() {
+        Scoreboard scoreboard = new Scoreboard();
+
+        assertTrue(scoreboard instanceof Scoreboard); //team is not null (Team)
+    }
 
 }
