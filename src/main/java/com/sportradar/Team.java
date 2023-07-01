3225fe4a39f6596ef82be0488d5385ad87096cf1
andrzej.salamon@gmail.com <andrzej.salamon@gmail.com>
2023-07-01 19:56:59 +0200

    Team class

--- src/main/java/com/sportradar/Team.java
+++ src/main/java/com/sportradar/Team.java
@@ -1,4 +1,18 @@
 package com.sportradar;
 
 public class Team {
+    public Team() {
+
+    }
+
+    public Team(String nameTeamExample) {
+
+    }
+
+    public void setName(String nameTeamExample) {
+    }
+
+    public String getName() {
+        return "";
+    }
 }
